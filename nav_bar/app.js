aa59c8af395612fce6330b1820f6b7d6e22a1b20
mondher04000 <m_mameri@estin.dz>
2023-03-23 22:46:38 +0100

    update app.js
    update

--- nav_bar/app.js
+++ nav_bar/app.js
@@ -5,7 +5,9 @@ links = document.querySelector(".links");
 navToglle.addEventListener('click', () => {
     if (links.style.display == "block") {
         links.style.display = "none";
+        navToglle.style.transform = "unset";
     }else {
         links.style.display = "block";
+        navToglle.style.transform = "rotate(90deg)";
     }
 })
