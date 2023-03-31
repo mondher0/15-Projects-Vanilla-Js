e94a99aec8fe7c38fe9fb0f3141d577c3cf6fc39
mondher04000 <m_mameri@estin.dz>
2023-03-31 18:38:50 +0100

    update

--- menu/app.js
+++ menu/app.js
@@ -18,7 +18,7 @@ const getFood = async (url) => {
               <div class="item-info">
                   <header>
                   <h4>${item.title}</h4>
-                  <h4 class="price">${item.price}</h4>
+                  <h4 class="price">$${item.price}</h4>
                   </header>
                   <p class="item-text">${item.desc}</p>
               </div>
